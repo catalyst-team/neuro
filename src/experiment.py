765b9c6ac2a7e91dd5631db4f98bb39556214733
Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
2020-04-05 10:41:58 +0300

    Update experiment.py

--- src/experiment.py
+++ src/experiment.py
@@ -11,7 +11,8 @@ from catalyst.data import (
     ListDataset,
     ReaderCompose,
 )
-
+from catalyst.data import Augmentor
+from torchvision import transforms
 from catalyst.dl import ConfigExperiment
 
 
@@ -30,9 +31,13 @@ class Experiment(ConfigExperiment):
         return model_
     def get_transforms(self, stage: str = None, mode: str = None):
         if mode == "train":
-            return torch.from_numpy
+            Augmentor1 = Augmentor(dict_key = "images" ,augment_fn=lambda x: torch.from_numpy(x).float())
+            Augmentor2 = Augmentor(dict_key = "labels" ,augment_fn=lambda x: torch.from_numpy(x).float())
+            return transforms.Compose([Augmentor1, Augmentor2])
         elif mode == "valid":
-            return torch.from_numpy
+            Augmentor1 = Augmentor(dict_key = "images" ,augment_fn=lambda x: torch.from_numpy(x).float())
+            Augmentor2 = Augmentor(dict_key = "labels" ,augment_fn=lambda x: torch.from_numpy(x).float())
+            return transforms.Compose([Augmentor1, Augmentor2])
         
 
     def get_datasets(
@@ -43,9 +48,11 @@ class Experiment(ConfigExperiment):
         in_csv_train: str = None,
         in_csv_valid: str = None,
     ):
-
-        df_train = pd.read_csv(in_csv_train)
-        df_valid = pd.read_csv(in_csv_valid)
+        train_folds = [1] * 60
+        valid_folds = [2] * 20
+        infer_folds = [3] * 21
+        df, df_train, df_valid, df_infer = read_csv_data(in_csv=in_csv, train_folds = train_folds, valid_folds = valid_folds, infer_folds = infer_folds)
+                                                                                                             
         datasets = collections.OrderedDict()
         open_fn = ReaderCompose(
             readers=[
