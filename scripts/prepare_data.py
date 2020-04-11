e54e13060b50c21acf17f8b1bc0087f3d7479877
Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
2020-04-11 09:17:50 +0300

    Update prepare_data.py

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,17 +1,11 @@
 import pathlib
 import pandas as pd
-from sklearn import model_selection
 import os
-import shutil
-import gzip
 
 def find_sample(path):
     labels_data = {"images":[],"labels":[]}
     for case in os.listdir(path):
         case_folder = os.path.join(path, case)
-        if case.endswith(".tar.gz"):
-            shutil.unpack_archive(os.path.join(path, case), path)
-            os.remove(os.path.join(path, case))
         for person in os.listdir(case_folder):
             if person in ["._.DS_Store",".DS_Store"]:
                 continue
@@ -28,19 +22,9 @@ def find_sample(path):
 
 
 def main(
-    datapath: str = "/home/Bekovmi/neuro-1/scripts/data/Mindbonggle_101", valid_size: float = 0.2, random_state: int = 42
+    datapath: str = "../data/dataverse_files", valid_size: float = 0.2, random_state: int = 42
 ):
     dataframe = find_sample(datapath)
-
-    df_train, df_valid = model_selection.train_test_split(
-        dataframe,
-        test_size=valid_size,
-        random_state=random_state,
-        shuffle=False
-    )
-    for source, mode in zip((df_train, df_valid), ("train", "valid")):
-        source.to_csv(f"data/dataset_{mode}.csv", index=False)
-
-
+    dataframe.to_csv(f"data/dataset.csv", index=False)
 if __name__ == "__main__":
     main()
