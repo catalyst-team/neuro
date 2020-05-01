7c8d0698ca77019054c6a804abe28803f2d88939
Sergey Kolesnikov <scitator@gmail.com>
2020-05-01 22:45:42 +0300

    Demo/checks (#20)
    * Update prepare_data.py (#10)
    
    update dict
    
    * upd
    
    * Dev (#17) (#18)
    
    * refactor code
    
    * refactor code
    
    Co-authored-by: Дмитрий Блеклов <dmitrijbleklov@MacBook-Pro-Dmitrij.local>
    
    Co-authored-by: Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
    Co-authored-by: Дмитрий Блеклов <dmitrijbleklov@MacBook-Pro-Dmitrij.local>
    
    * +
    
    * Demo/checks (#19)
    
    * upd
    
    * +
    
    * Update README.md
    
    Co-authored-by: Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
    Co-authored-by: Дмитрий Блеклов <dmitrijbleklov@MacBook-Pro-Dmitrij.local>

---
+++ training/reader.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from catalyst.data import ReaderSpec
+
+
+class NiftiReader(ReaderSpec):
+    """
+    Nifti reader abstraction for NeuroImaging.
+    Reads nifti images from a `csv` dataset.
+    """
+
+    def __init__(self, input_key: str, output_key: str, rootpath: str = None):
+        """
+        Args:
+            input_key (str): key to use from annotation dict
+            output_key (str): key to use to store the result
+            rootpath (str): path to images dataset root directory
+                (so your can use relative paths in annotations)
+            grayscale (bool): flag if you need to work only
+                with grayscale images
+        """
+        super().__init__(input_key, output_key)
+        self.rootpath = rootpath
+
+    def __call__(self, element):
+        """
+        Reads a row from your annotations dict with filename and
+        transfer it to an image
+        Args:
+            element: elem in your dataset.
+        Returns:
+            np.ndarray: Image
+        """
+        image = np.frombuffer(element[self.input_key], dtype=np.uint8)
+        output = {self.output_key: image}
+        return output
