2aeb0d864195d3b0adc4d490c977f8dd5c4fd5eb
Sergey Kolesnikov <scitator@gmail.com>
2020-05-01 18:16:38 +0300

    codestyle update

--- src/reader.py
+++ src/reader.py
@@ -1,22 +1,23 @@
-from catalyst.data import ReaderSpec
-import nibabel as nib
-import os
-import numpy as np
-import math
-import ast 
+import ast
 
+import nibabel as nib
 import numpy as np
+
+from catalyst.data import ReaderSpec
+
 
 class NiftiReader_Image(ReaderSpec):
     """
-    Nifti reader abstraction for NeuroImaging. Reads nifti images from a `csv` dataset.
+    Nifti reader abstraction for NeuroImaging.
+    Reads nifti images from a `csv` dataset.
     """
+
     def __init__(
         self,
         input_key: str,
         coords: list,
         output_key: str,
-        rootpath: str = None
+        rootpath: str = None,
     ):
         """
         Args:
@@ -32,7 +33,8 @@ class NiftiReader_Image(ReaderSpec):
         self.coords = coords
 
     def __call__(self, element):
-        """Reads a row from your annotations dict with filename and
+        """
+        Reads a row from your annotations dict with filename and
         transfer it to an image
         Args:
             element: elem in your dataset.
@@ -41,32 +43,36 @@ class NiftiReader_Image(ReaderSpec):
         """
         coords = ast.literal_eval(element[self.coords])
         image_name = str(element[self.input_key])
-        subvolume_shape = np.array([64,64,64])
-        x = np.zeros((1,subvolume_shape[0],subvolume_shape[1],subvolume_shape[2]))
+        subvolume_shape = np.array([64, 64, 64])
+        x = np.zeros(
+            (1, subvolume_shape[0], subvolume_shape[1], subvolume_shape[2])
+        )
         img = nib.load(image_name)
         img = img.get_fdata(dtype=np.float32)
-        img = (img - img.min())/(img.max()-img.min())
-        img = img*255.0
-        new_img = np.zeros([256,256,256])
-        new_img[:img.shape[0], :img.shape[1], :img.shape[2]] = img
-        x[0,:subvolume_shape[0],:subvolume_shape[1],:subvolume_shape[2]] = new_img[
-                    coords[0][0]:coords[0][1], 
-                    coords[1][0]:coords[1][1], 
-                    coords[2][0]:coords[2][1]]
+        img = (img - img.min()) / (img.max() - img.min())
+        img = img * 255.0
+        new_img = np.zeros([256, 256, 256])
+        new_img[: img.shape[0], : img.shape[1], : img.shape[2]] = img
+        x[
+            0, : subvolume_shape[0], : subvolume_shape[1], : subvolume_shape[2]
+        ] = new_img[
+            coords[0][0] : coords[0][1],
+            coords[1][0] : coords[1][1],
+            coords[2][0] : coords[2][1],
+        ]
 
         output = {self.output_key: x.astype(np.float32)}
         return output
 
+
 class NiftiReader_Mask(ReaderSpec):
     """
-    Nifti reader abstraction for NeuroImaging. Reads nifti images from a `csv` dataset.
+    Nifti reader abstraction for NeuroImaging.
+    Reads nifti images from a `csv` dataset.
     """
+
     def __init__(
-        self,
-        input_key: str,
-        coords,
-        output_key: str,
-        rootpath: str = None
+        self, input_key: str, coords, output_key: str, rootpath: str = None
     ):
         """
         Args:
@@ -82,7 +88,8 @@ class NiftiReader_Mask(ReaderSpec):
         self.coords = coords
 
     def __call__(self, element):
-        """Reads a row from your annotations dict with filename and
+        """
+        Reads a row from your annotations dict with filename and
         transfer it to an image
         Args:
             element: elem in your dataset.
@@ -90,15 +97,24 @@ class NiftiReader_Mask(ReaderSpec):
             np.ndarray: Image
         """
         coords = ast.literal_eval(element[self.coords])
-        subvolume_shape = np.array([64,64,64])
+        subvolume_shape = np.array([64, 64, 64])
         image_name = str(element[self.input_key])
         label = np.load(image_name, allow_pickle=True)
-        y = np.zeros([label.shape[0],subvolume_shape[0], subvolume_shape[1], subvolume_shape[2]])
-        y[:,:subvolume_shape[0],:subvolume_shape[1],:subvolume_shape[2]] = label[:,
-                    coords[0][0]:coords[0][1], 
-                    coords[1][0]:coords[1][1],  
-                    coords[2][0]:coords[2][1]]
+        y = np.zeros(
+            [
+                label.shape[0],
+                subvolume_shape[0],
+                subvolume_shape[1],
+                subvolume_shape[2],
+            ]
+        )
+        y[
+            :, : subvolume_shape[0], : subvolume_shape[1], : subvolume_shape[2]
+        ] = label[
+            :,
+            coords[0][0] : coords[0][1],
+            coords[1][0] : coords[1][1],
+            coords[2][0] : coords[2][1],
+        ]
         output = {self.output_key: y}
         return output
-
-
