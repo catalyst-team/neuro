9c99b18ef2e822e3b691de286eec209bc8b8292e
Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
2020-05-31 21:03:29 +0300

    Update __init__.py

--- training/__init__.py
+++ training/__init__.py
@@ -1,6 +1,4 @@
-from catalyst.dl import registry
-
-from catalyst.dl SupervisedRunner as Runner  # noqa: F401
+from catalyst.dl import registry, SupervisedRunner as Runner  # noqa: F401
 
 from .experiment import Experiment  # noqa: F401
 
