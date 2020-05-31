d9d9452b3678aff6a530f6aaa85fbef007adc6fc
Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
2020-05-31 20:57:52 +0300

    Update __init__.py

--- training/__init__.py
+++ training/__init__.py
@@ -1,6 +1,9 @@
-from catalyst.dl import registry, 
+from catalyst.dl import registry
+
 from catalyst.dl SupervisedRunner as Runner  # noqa: F401
+
 from .experiment import Experiment  # noqa: F401
+
 from .model import UNet
 
 registry.Model(UNet)
